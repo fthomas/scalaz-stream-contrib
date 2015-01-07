52568d0c0885785eafab8e292693d944659dbc6f
Frank S. Thomas <frank@timepit.eu>
2015-01-07 21:23:06 +0100

    Add tripleRunningMean

--- src/main/scala/eu/timepit/scalaz/stream/contrib/numericC.scala
+++ src/main/scala/eu/timepit/scalaz/stream/contrib/numericC.scala
@@ -10,6 +10,15 @@ object numericC {
     sliding[F](n).map(w => F.div(w.sum, nf))
   }
 
+  // https://climategrog.wordpress.com/2013/05/19/triple-running-mean-filters/
+  def tripleRunningMean[F](n: Int)(implicit F: Fractional[F]): Process1[F, F] = {
+    val f = 1.3371
+    val nd = n.toDouble
+    val n2 = (nd / f).toInt
+    val n3 = (nd / f / f).toInt
+    movingAverage(n) |> movingAverage(n2) |> movingAverage(n3)
+  }
+
   def prefixMeans[F](implicit F: Fractional[F]): Process1[F, F] = {
     val div = (F.div _).tupled
     prefixSums[F].zipWithIndex[F].drop(1).map(div)
