a93478ce2b8ce054b6427ec4b5e474e991ac7053
Frank S. Thomas <frank@timepit.eu>
2015-04-07 19:58:05 +0200

    Add simple rng module

---
+++ src/main/scala/eu/timepit/scalaz/stream/contrib/rng.scala
@@ -0,0 +1,32 @@
+package eu.timepit.scalaz.stream.contrib
+
+import com.nicta.rng.Rng
+
+import scalaz.concurrent.Task
+import scalaz.stream.Process
+import scalaz.stream.Process._
+import scalaz.~>
+
+object rng {
+  def int: Process[Rng, Int] =
+    repeatEval(Rng.int)
+
+  def long: Process[Rng, Long] =
+    repeatEval(Rng.long)
+
+  def float: Process[Rng, Float] =
+    repeatEval(Rng.float)
+
+  def double: Process[Rng, Double] =
+    repeatEval(Rng.double)
+
+  implicit class ProcessRngSyntax[O](val self: Process[Rng, O]) extends AnyVal {
+    def toSource: Process[Task, O] =
+      self.translate(rngToTask)
+  }
+
+  val rngToTask: Rng ~> Task = new ~>[Rng, Task] {
+    def apply[A](rng: Rng[A]): Task[A] =
+      Task.delay(rng.run.unsafePerformIO())
+  }
+}
