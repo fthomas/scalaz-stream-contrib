fd28d43c6c3e2066de7924e4579a21b266b586b0
Frank S. Thomas <frank@timepit.eu>
2015-04-09 07:33:32 +0200

    Add Catchable[Rng] instance

--- src/main/scala/eu/timepit/scalaz/stream/contrib/rng.scala
+++ src/main/scala/eu/timepit/scalaz/stream/contrib/rng.scala
@@ -5,9 +5,12 @@ import com.nicta.rng.Rng
 import scalaz.concurrent.Task
 import scalaz.stream.Process
 import scalaz.stream.Process._
-import scalaz.~>
+import scalaz.{ Catchable, \/, ~> }
 
 object rng {
+  def boolean: Process[Rng, Boolean] =
+    repeatEval(Rng.boolean)
+
   def int: Process[Rng, Int] =
     repeatEval(Rng.int)
 
@@ -28,5 +31,11 @@ object rng {
   val rngToTask: Rng ~> Task = new ~>[Rng, Task] {
     def apply[A](rng: Rng[A]): Task[A] =
       Task.delay(rng.run.unsafePerformIO())
+  }
+
+  implicit val rngCatchable: Catchable[Rng] = new Catchable[Rng] {
+    def fail[A](err: Throwable): Rng[A] = ???
+
+    def attempt[A](f: Rng[A]): Rng[Throwable \/ A] = f.map(\/.right)
   }
 }
